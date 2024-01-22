00d0514430a6192e9d3a8d6d2a03473ce42c6af8
Amit Gupta <120751752+amit-gupta120@users.noreply.github.com>
2024-01-22 16:25:19 +0530

    Add files via upload
    Commit UI Completed

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Post.scss";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
@@ -6,8 +6,12 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
 
 const Post = ({ post }) => {
+  const [commentOpen, setCommentOpen] = useState(false);
+
+  const liked = false; //Temporary
   return (
     <div className="post">
       <div className="container">
@@ -19,8 +23,9 @@ const Post = ({ post }) => {
               <Link
                 to={`/profile/${post.userId}`}
                 style={{ textDecoration: "none", color: "inherit" }}
-              ></Link>
-              <span className="name">{post.name}</span>
+              >
+                <span className="name">{post.name}</span>
+              </Link>
               <span className="date">1 min ago</span>
             </div>
           </div>
@@ -30,7 +35,21 @@ const Post = ({ post }) => {
           <p>{post.desc}</p>
           <img src={post.img} alt="" />
         </div>
-        <div className="info"></div>
+        <div className="info">
+          <div className="item">
+            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+            12M likes
+          </div>
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+            <TextsmsOutlinedIcon />
+            100K comments
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon />
+            Share
+          </div>
+        </div>
+        {commentOpen && <Comments />}
       </div>
     </div>
   );
